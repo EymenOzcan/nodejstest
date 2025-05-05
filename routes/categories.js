ad272508530f45f898090bdb989a4b6766b918d1
EymenOzcan <eymennaciozcan@gmail.com>
2025-05-05 15:05:11 +0300

    routing altyapısı kuruldu #2

---
+++ routes/categories.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+const isAuthhenticated = true;
+router.all("*", (req, res, next) => {
+  if (isAuthhenticated) {
+    next();
+  } else {
+    res.json({ success: false, error: "Not Authhenticated" });
+  }
+});
+router.get("/", function (req, res, next) {
+  res.json({ success: true });
+});
+
+module.exports = router;
