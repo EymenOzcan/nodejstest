2c3ee93137f1db737c6055aa5a576532e9c2b534
EymenOzcan <eymennaciozcan@gmail.com>
2025-05-23 13:11:52 +0300

    #5 user Auth yapısı kuruldu

--- config/index.js
+++ config/index.js
@@ -3,4 +3,10 @@ module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || "debug",
   CONNECTION_STRING:
     process.env.CONNECTION_STRING || "mongodb://127.0.0.1:27017/nodejstest",
+  JWT: {
+    SECRET: "123456",
+    EXPIRE_TIME: !isNaN(parseInt(process.env.TOKEN_EXPIRE_TIME))
+      ? parseInt(process.env.TOKEN_EXPIRE_TIME)
+      : 24 * 60 * 60,
+  },
 };
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ const CustomError = require("../lib/Error");
 const Enum = require("../config/Enum");
 const UserRoles = require("../db/models/UserRoles");
 const Roles = require("../db/models/Roles");
+const config = require("../config");
+const jwt = require("jwt-simple");
 var router = express.Router();
 
 router.get("/", async (req, res, next) => {
@@ -127,15 +129,17 @@ router.put("/update", async (req, res) => {
       let userRoles = await UserRoles.find({
         user_id: body._id,
       });
-      let removedRoles = userRoles.filter((x) => !body.roles.includes(x.role_id)
+      let removedRoles = userRoles.filter(
+        (x) => !body.roles.includes(x.role_id)
       );
       let newRoles = body.roles.filter(
         (x) => !userRoles.map((r) => r.role_id).includes(x)
       );
 
       if (removedRoles.length > 0) {
-        await UserRoles.deleteMany({_id: { $in: removedRoles.map((x) => x._id.toString()) },
-      });
+        await UserRoles.deleteMany({
+          _id: { $in: removedRoles.map((x) => x._id.toString()) },
+        });
       }
 
       if (newRoles.length > 0) {
@@ -243,6 +247,37 @@ router.post("/register", async (req, res) => {
       .json(
         Response.successResponse({ succes: true }, Enum.HTTP_CODES.CREATED)
       );
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  }
+});
+router.post("/auth", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    Users.validateFieldsBeforeAuth(email, password);
+    let user = await Users.findOne({ email });
+    if (!user)
+      throw new CustomError(
+        Enum.HTTP_CODES.UNAUTHORIZED,
+        "Email Veya Şifre Hatalı"
+      );
+    if (!user.validPassword(password))
+      throw new CustomError(
+        Enum.HTTP_CODES.UNAUTHORIZED,
+        "Email Veya Kullanıcı Adı Hatalı"
+      );
+    let payload = {
+      id: user._id,
+      exp: parseInt(Date.now() / 1000) * config.JWT.EXPIRE_TIME,
+    };
+    let userData = {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+    };
+    let token = jwt.encode(payload, config.JWT.SECRET);
+    res.json(Response.successResponse({ token, user: { userData } }));
   } catch (err) {
     let errorResponse = Response.errorResponse(err);
     res.status(errorResponse.code).json(errorResponse);
